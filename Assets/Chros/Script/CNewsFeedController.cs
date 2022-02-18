24c060f922956f2eac9ac14a7f5cf5b7c030c730
Chros <chroswang@gmail.com>
2022-02-18 10:57:16 -0500

    Spreadsheetplugin

---
+++ Assets/Chros/Script/CNewsFeedController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CNewsFeedController : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateFeeds()
+    {
+        //This will be the function to generate three news feed base on current game state and conspiracy level
+        
+    }
+}
