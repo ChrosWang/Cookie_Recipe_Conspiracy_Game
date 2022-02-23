fdf8aca1317c2d3350c1cb602497ca55ec213a5c
nyusha-jpg <ai24@nyu.edu>
2022-02-23 01:27:06 -0500

    New article prefab
    Using TextMeshPro instead of regular Text

---
+++ Assets/ArticleData.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArticleData : MonoBehaviour
+{
+    //public TMP_Text title;
+    //public TMP_Text date;
+    //public TMP_Text body;
+    //public bool paywall;
+    //public int multiplier;
+    //public int score;
+    //public int ID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(title);
+        //Debug.Log(date);
+        //Debug.Log(body);
+        //Debug.Log(multiplier);
+        //Debug.Log(score);
+        //Debug.Log(ID);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
