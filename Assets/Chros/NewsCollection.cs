ca8ea3fc62576cb225ea33540bb1dfb281a8bd55
Chros <chroswang@gmail.com>
2022-02-24 17:11:26 -0500

    Search Engine Rough

--- Assets/Chros/NewsCollection.cs
+++ Assets/Chros/NewsCollection.cs
@@ -12,11 +12,70 @@ public class NewsArticle
     public bool Paywall;
     public int Multipiler;
     public int Score;
-    public int ID;
+    public int Priority;
 }
 
 [System.Serializable]
 public class NewsCollection
 {
-    public NewsArticle[] News; 
+    public NewsArticle[] News;
+    public int[] SearchList = new int[25];
+    public int searchResultStat; 
+
+    public void SearchForKeyWord(string keyword)
+    {
+        Debug.Log("Start Searching");
+        for(int i = 0; i < SearchList.Length; i++)
+        {
+            SearchList[i] = 0;  
+        }
+        searchResultStat = 0; 
+
+        Debug.Log("Compelete Clearing the whole list"); 
+        //Clear out the search list
+        int currentIndex = 0; 
+        for (int i = 0; i < News.Length; i++)
+        {
+            if ((News[i].Title.ToLower().IndexOf(keyword.ToLower()) >= 0) || (News[i].Body.ToLower().IndexOf(keyword.ToLower()) >= 0))
+            {
+                Debug.Log("Found a matching one with index " + i); 
+                SearchList[currentIndex] = i;
+                currentIndex++;
+                searchResultStat++;
+            }
+            Debug.Log("index " + i + " not matched"); 
+        }
+
+        Debug.Log("Complete Searching, now start to sort the list");
+        Debug.Log("Found " + SearchList.Length); 
+        //search key word
+        SortSearchListPriority(); 
+    }
+
+    void SortSearchListPriority()
+    {
+        int n = SearchList.Length;
+        Debug.Log("Sorting the list");
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (RetrieveNewsArticle(j).Priority > RetrieveNewsArticle(j).Priority)
+                {
+                    // swap temp and SearchList[i]
+                    int temp = SearchList[j];
+                    SearchList[j] = SearchList[j + 1];
+                    SearchList[j + 1] = temp;
+                }
+            }
+        }
+        Debug.Log("Finish Sorting"); 
+    }
+
+    public NewsArticle RetrieveNewsArticle (int index)
+    {
+        return News[SearchList[index]];
+    }
+
 }
+
--- Assets/Chros/NewsLoader.cs
+++ Assets/Chros/NewsLoader.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class NewsLoader : MonoBehaviour
 {
     public TextAsset SpreadSheetJSON;
-    public NewsCollection myNewsCollection = new NewsCollection(); 
+    public NewsCollection myNewsCollection = new NewsCollection();
+    public Button SearchButton;
+    public Text textField;
+    public TMP_Text SearchResult1Title;
+    public TMP_Text SearchResult2Title;
+    public TMP_Text SearchResult3Title;
+
+    public TMP_Text SearchResult1Body;
+    public TMP_Text SearchResult2Body;
+    public TMP_Text SearchResult3Body;
+
+    public TMP_Text SearchResultStats;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +27,30 @@ public class NewsLoader : MonoBehaviour
         Debug.Log(myNewsCollection.News[0].Date);
         Debug.Log(myNewsCollection.News[0].Body);
 
-    }
+        SearchButton.onClick.AddListener(() => SearchMyCollection(textField.text)); 
 
+    }
+    
+    void SearchMyCollection(string keyword)
+    {
+        Debug.Log("Search with " + keyword); 
+        myNewsCollection.SearchForKeyWord(keyword);
+        SearchResultStats.text = "Found " + myNewsCollection.searchResultStat.ToString() + " matched results."; 
+        RetrieveTopThree();
+    }
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void RetrieveTopThree ()
+    {
+        SearchResult1Title.text = myNewsCollection.RetrieveNewsArticle(0).Title + " " + myNewsCollection.RetrieveNewsArticle(0).Date;
+        SearchResult1Body.text = myNewsCollection.RetrieveNewsArticle(0).Body;
+        SearchResult2Title.text = myNewsCollection.RetrieveNewsArticle(1).Title + " " + myNewsCollection.RetrieveNewsArticle(1).Date;
+        SearchResult2Body.text = myNewsCollection.RetrieveNewsArticle(1).Body;
+        SearchResult3Title.text = myNewsCollection.RetrieveNewsArticle(2).Title + " " + myNewsCollection.RetrieveNewsArticle(2).Date;
+        SearchResult3Body.text = myNewsCollection.RetrieveNewsArticle(2).Body;
     }
 }
