a5165220a8eb650e8911d598b422caa9afaeb0a4
nyusha-jpg <ai24@nyu.edu>
2022-03-25 09:52:19 -0400

    Change tabs

---
+++ Assets/Nyusha/StateMachine.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StateMachine : MonoBehaviour
+{
+
+    //singleton setup for nerds and dweebs
+    private static StateMachine instance;
+
+    public static StateMachine FindInstance()
+    {
+        return instance;
+    }
+
+
+    //Declare all pages here
+
+    public GameObject socialCanvas;
+    public GameObject newsfeedCanvas;
+    public GameObject chatCanvas;
+
+    //Buttons
+
+    public Button social;
+    public Button newsfeed;
+    public Button chat;
+
+    public enum Tab
+    {
+        Social,
+        Newsfeed,
+        Chat
+    }
+
+    public Tab currentTab; //tracks the current tab
+
+    void Awake()
+    {
+        //singleton for dumb idiot computers
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else if (instance == null)
+        {
+            DontDestroyOnLoad(this);
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ///ChangeTab(Tab.Social); //start with Social
+
+        social.onClick.AddListener(() => ChangeTab(Tab.Social));
+        //if button social is clicked, call function ChangeTab(Tab.Social)
+
+        newsfeed.onClick.AddListener(() => ChangeTab(Tab.Newsfeed));
+        chat.onClick.AddListener(() => ChangeTab(Tab.Chat));
+    }
+
+    public void ChangeTab(Tab newTab)
+    {
+        currentTab = newTab;
+        switch (newTab) //check the newTab
+        { //if the newTab is...
+            case Tab.Social:
+                socialCanvas.SetActive(true);
+                newsfeedCanvas.SetActive(false);
+                chatCanvas.SetActive(false);
+                Debug.Log("Change to tab social");
+                break;
+            case Tab.Newsfeed:
+                socialCanvas.SetActive(false);
+                newsfeedCanvas.SetActive(true);
+                chatCanvas.SetActive(false);
+                Debug.Log("Change to tab newsfeed");
+                break;
+            case Tab.Chat:
+                socialCanvas.SetActive(false);
+                newsfeedCanvas.SetActive(false);
+                chatCanvas.SetActive(true);
+                Debug.Log("Change to tab chat");
+                break;
+            default:
+                Debug.Log("this state doesn't exist");
+                break;
+        }
+    }
+}
