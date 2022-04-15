bf9006d74d29739b0dff8c0cfdaa8ea0fbf0e70a
Swathi Sambasivam <Ss15049@nyu.edu>
2022-04-15 09:52:43 -0400

    Trying animations and failing

---
+++ Assets/Swathi/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    
+public Animator transition;
+    public GameObject chatCanvas;
+public void NewsfeedLoader()
+    {
+       Debug.Log("Newsfeed Animation");
+}
+    
+    // void Update()
+    // {
+    //     if(chatCanvas)
+    //     {
+    //         if(Input.GetMouseButtonDown(0)){
+    //     //transition.SetTrigger("Chat Exit_Trigger");
+    //     Debug.Log("it leave baby");
+    // }}}
+    
+}
