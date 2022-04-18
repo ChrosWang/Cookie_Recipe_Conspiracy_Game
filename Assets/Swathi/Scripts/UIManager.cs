b582c987386ec190284c158cd051e6b1dde2f4c7
Swathi Sambasivam <Ss15049@nyu.edu>
2022-04-17 23:05:52 -0400

    updating images and taskbar

--- Assets/Swathi/Scripts/UIManager.cs
+++ Assets/Swathi/Scripts/UIManager.cs
@@ -7,9 +7,24 @@ public class UIManager : MonoBehaviour
     
 public Animator transition;
     public GameObject chatCanvas;
-public void NewsfeedLoader()
+    public GameObject Article;
+     public CanvasGroup Title;
+    public CanvasGroup Bodytext;
+
+    public CanvasGroup Image;
+
+
+public void ArticlePageLoader()
     {
-       Debug.Log("Newsfeed Animation");
+       Debug.Log("ArticleAnim");
+       Article.LeanMove(new Vector2(8,-2),1).setEaseInOutQuart();
+       Title.alpha = 0;
+       Title.LeanAlpha(1,1.5f).setEaseInExpo();
+       Bodytext.alpha = 0;
+       Bodytext.LeanAlpha(1,2f).setEaseInBounce();
+      Image.alpha = 0;
+       Image.LeanAlpha(1,2f).setEaseInElastic();
+       
 }
     
     // void Update()
