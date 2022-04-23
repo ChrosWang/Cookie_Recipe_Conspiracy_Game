cc9b364e5ee664b6f0d0fc365332076ee635312b
Chros <chroswang@gmail.com>
2022-04-23 14:39:06 -0400

    Auto stash before rebase of "origin/swathi"

--- Assets/Chros/ChatCollection.cs
+++ Assets/Chros/ChatCollection.cs
@@ -2,6 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+
+public class ChatMessage
+{
+    int GameState;
+    int Order;
+    string Character;
+    string Text;
+    string TimeSent;
+}
 public class ChatCollection
 {
     string ContactName;
