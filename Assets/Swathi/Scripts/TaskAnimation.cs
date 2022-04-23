a2279a73bda418307a121b75e0c4aec6faffbcec
Swathi Sambasivam <Ss15049@nyu.edu>
2022-04-23 14:36:48 -0400

    Leaning tweening

--- Assets/Swathi/Scripts/TaskAnimation.cs
+++ Assets/Swathi/Scripts/TaskAnimation.cs
@@ -6,14 +6,25 @@ public class TaskAnimation : MonoBehaviour
 {
    public GameObject box;
    public GameObject taskbar;
-   public CanvasGroup MixerText;
+   public Color Nocolor;
+   public Color fullcolor; 
+   
 
 
     public void taskanimation()
     {
-        
+        box.LeanScaleY(30,1f).setEaseInOutQuart();
+        box.LeanScaleX(30,2f).setEaseInBack();
+       // MixerText.alpha = 1;
+       // MixerText.LeanAlpha(0,1.5f).setEaseInExpo();
+       // Logos.alpha = 0;
+       // Logos.LeanAlpha(1,1.5f).setEaseInExpo();
+       LeanTween.value(box, fullcolor, Nocolor, 5f ).setEasePunch();
+       LeanTween.value(taskbar, Nocolor, fullcolor, 5f ).setEasePunch();
+
 
 
     }
+    
 
 }
