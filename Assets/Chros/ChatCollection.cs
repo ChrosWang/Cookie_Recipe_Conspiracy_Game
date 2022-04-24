3ab8fd145d24c170a2547e86bc98ac5aa25d53cb
Chros <chroswang@gmail.com>
2022-04-24 01:42:21 -0400

    chat attempt

--- Assets/Chros/ChatCollection.cs
+++ Assets/Chros/ChatCollection.cs
@@ -6,14 +6,50 @@ using UnityEngine;
 
 public class ChatMessage
 {
-    int GameState;
-    int Order;
-    string Character;
-    string Text;
-    string TimeSent;
+    public int GameState;
+    public int Order;
+    public  string Character;
+    public string Text;
+    public string TimeSent;
+    public string Keywords;
 }
+
+//[System.Serializable]
+public class Conversation
+{
+    public ChatMessage[] chatmessages = new ChatMessage[99];
+    public string character;
+    public int GameState;
+    public int Order;
+    public void setMessage(ChatMessage TargetChat)
+    {
+        Debug.Log("Step 3");
+        chatmessages[chatmessages.Length - 1] = TargetChat; 
+    }
+
+}
+
+[System.Serializable]
 public class ChatCollection
 {
-    string ContactName;
-    string[][] ChatList;
+    public ChatMessage[] chatmessages; 
+
+    
+
+    
+}
+
+[System.Serializable]
+public class ConvCollection
+{
+    public Conversation[] conversations = new Conversation[99];
+    public void initializeConv ()
+    {
+
+    }
+    public void TakeMessages (int order, ChatMessage TargetChat)
+    {
+        Debug.Log("Step 2 putting order " + order +" in " + conversations.Length);
+        conversations[order].setMessage(TargetChat); 
+    }
 }
