76539285efdbe9fdecbc52f566cd6b43caae3c8d
Chros <chroswang@gmail.com>
2022-05-17 15:20:36 -0400

    merge success

--- Assets/Nyusha/PopUps.cs
+++ Assets/Nyusha/PopUps.cs
@@ -6,9 +6,9 @@ using UnityEngine.UI;
 public class PopUps : MonoBehaviour
 {
 
-    public GameObject art
-    public GameObject art1
-    public GameObject art2
+    public GameObject art;
+    public GameObject art1;
+    public GameObject art2;
 
     // Start is called before the first frame update
     void Start()
