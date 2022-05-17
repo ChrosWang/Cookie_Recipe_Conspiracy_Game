64722c74df6532d79fd01db5758ca0d4b7fa4932
Nyusha <ai24@nyu.edu>
2022-05-17 17:25:13 -0400

    lean tween and WIP pop ups

--- Assets/Nyusha/PopUps.cs
+++ Assets/Nyusha/PopUps.cs
@@ -6,19 +6,59 @@ using UnityEngine.UI;
 public class PopUps : MonoBehaviour
 {
 
-    public GameObject art
-    public GameObject art1
-    public GameObject art2
+    public GameObject[] articles; //list of pop ups
+    int popUps; //how many pop ups are on screen
+    float timer; //timer countdown
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        popUps = 0; //zero pop ups on screen initially
+        timer = 2f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Debug.Log(timer);
+        if ((Input.GetMouseButtonDown(0)) && (popUps < 4)) //if clicking and less than 4 pop ups
+        {
+            ClickToPopUp();
+        }
+
+        if (popUps >= 5) //if five or more pop ups on screen
+
+            if (timer > 0) //if timer is greater than zero
+            {
+                timer = timer - Time.deltaTime; //decrease the timer amount
+            }
+
+            if (timer < 0) //if timer reaches zero
+            {
+                AutoPop();
+                timer = 2f; //reset it back to 2 seconds
+            }
+        }
+
+    void ClickToPopUp() //the first stage, manual control over pop ups
+    {
+        //Pop Up appears
+        Debug.Log(popUps);
+        articles[popUps].SetActive(true);
+        popUps++;
+       // Animation();
+    }
+  
+    void AutoPop()
+    {
+        articles[popUps].SetActive(true);
+        popUps++;
+        //Animation();
+    }
+
+    void Animation()
+    {
+
     }
 }
