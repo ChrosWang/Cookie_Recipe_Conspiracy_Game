5d516330bd4280d0e6ca9c851e180237c66be56b
Nyusha <ai24@nyu.edu>
2022-05-17 23:39:46 -0400

    Sounds, Audio, GS5 tweaks

---
+++ Assets/Nyusha/PlaySound.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public AudioSource art;
+    public AudioSource art1;
+    public AudioSource art2;
+    public AudioSource art3;
+    public AudioSource art4;
+    public AudioSource art5;
+    public AudioSource art6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Nyusha/PopUps.cs
+++ Assets/Nyusha/PopUps.cs
@@ -7,28 +7,33 @@ public class PopUps : MonoBehaviour
 {
 
     public GameObject[] articles; //list of pop ups
+    Color currentColor;
+    public GameObject myAudioSource;
+    public AudioClip[] sound;
+    public GameObject SoundSystem;
     int popUps; //how many pop ups are on screen
     float timer; //timer countdown
+    float quickTimer;
 
     // Start is called before the first frame update
     void Start()
     {
+       // currentColor = 
         popUps = 0; //zero pop ups on screen initially
         timer = 1f;
+        quickTimer = 0.3f;
+        articles[popUps].transform.localScale = Vector2.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        Debug.Log("Time: " + timer);
-
-        if ((Input.GetMouseButtonDown(0)) && (popUps < 4)) //if clicking and less than 4 pop ups
+        if ((Input.GetMouseButtonDown(0)) && (popUps < 3)) //if clicking and less than 4 pop ups
         {
             ClickToPopUp();
         }
 
-        if (popUps >= 4) //if more than four pop ups
+        if (popUps >= 3 && popUps < 5) //if popups are between
         {
             if (timer > 0) //if timer is greater than zero
             {
@@ -41,26 +46,82 @@ public class PopUps : MonoBehaviour
                 timer = 1f; //reset it back to 2 seconds
             }
         }
+
+        if (popUps >= 5) 
+        {
+            if (quickTimer > 0) //if timer is greater than zero
+            {
+                quickTimer = quickTimer - Time.deltaTime; //decrease the timer amount
+            }
+
+            if (quickTimer < 0) //if timer reaches zero
+            {
+                QuickAutoPop();
+                quickTimer = 0.3f; //reset
+            }
+        }
     }
 
     void ClickToPopUp() //the first stage, manual control over pop ups
     {
         //Pop Up appears
         Debug.Log("Popups =" + popUps);
+        articles[popUps].transform.localScale = Vector2.zero;
         articles[popUps].SetActive(true);
+        // PlaySound();
+        // Animation();
+        Effects();
         popUps++;
-       // Animation();
     }
   
     void AutoPop()
     {
+       articles[popUps].transform.localScale = Vector2.zero;
         articles[popUps].SetActive(true);
+        // PlaySound();
+        // Animation();
+        Effects();
         popUps++;
-        //Animation();
     }
 
-    void Animation()
-    {
+    void QuickAutoPop(){
+        articles[popUps].transform.localScale = Vector2.zero;
+        articles[popUps].SetActive(true);
+      //  PlaySound();
+      //  Animation();
+        Effects();
+        popUps++;
+    }
 
+    // void Animation()
+    // {
+    //     LeanTween.scale(articles[popUps], new Vector3(1f, 1f, 1f), 0.4f).setEase(LeanTweenType.easeInOutCubic);
+    // }
+
+    // void PlaySound()
+    // {
+    //     myAudioSource = Instantiate(myAudioSource);
+    //     myAudioSource.transform.SetParent(SoundSystem.transform);
+    //     myAudioSource.GetComponent<AudioSource>().clip = sound[popUps];
+    //     myAudioSource.GetComponent<AudioSource>().Play();
+    // }
+
+    void Effects()
+    {
+        //Animation
+        LeanTween.scale(articles[popUps], new Vector3(1f, 1f, 1f), 0.4f).setEase(LeanTweenType.easeInOutCubic);
+        //Audio
+        myAudioSource = Instantiate(myAudioSource);
+        myAudioSource.transform.SetParent(SoundSystem.transform);
+        myAudioSource.GetComponent<AudioSource>().clip = sound[popUps];
+        myAudioSource.GetComponent<AudioSource>().Play();
+        //Color Change
+         foreach (GameObject g in articles)
+        {
+            if (g.activeSelf)
+            {
+               // Debug.Log(g + "is active");
+            }
+        }
     }
 }
